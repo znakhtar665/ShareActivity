46b559e0d44bfeea1f131e88f19647a2b556cecc
Karl Morris <karl.morris@temple.edu>
2023-11-07 08:14:26 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -0,0 +1,21 @@
+package edu.temple.funwithintents
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // This view contains the text to share
+        val editText = findViewById<EditText>(R.id.editTextText)
+
+        // When the user clicks this button, share the text
+        findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+
+        }
+    }
+}
